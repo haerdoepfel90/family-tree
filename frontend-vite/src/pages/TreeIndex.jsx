aeadacff73a5e3437f8ba9cb933c54a6d290ca45
Micha Dössegger <michdos@bluewin.ch>
2026-08-27 22:39:41 +0200

    added styling to TreeIndex and hover styling in HomePage

--- frontend-vite/src/pages/TreeIndex.jsx
+++ frontend-vite/src/pages/TreeIndex.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router";
 import getPersonByID, { formatName } from "../lib/people";
+import "./TreeIndex.css";
 
 import { findRootFamilies } from "../lib/relatives";
 
@@ -20,16 +21,33 @@ export function TreeIndex() {
 	}, []);
 
 	return (
-		<ul className="tree-index">
+		<div className="tree-index__body">
+			<div className="tree-index__header">
+				Wurzelfamilien &middot; {`${roots.length}`}
+			</div>
 			{roots.map((root) => (
-				<li key={root.id}>
-					<Link to={`/tree/${root.id}`}>
-						{formatName(getPersonByID(root.partner1_id, peopleById), "normal")}{" "}
-						&amp;{" "}
-						{formatName(getPersonByID(root.partner2_id, peopleById), "normal")}
-					</Link>
-				</li>
+				<div className="tree-index__root-card" key={root.id}>
+					<div className="tree-index__root-card-left">
+						<div className="tree-index__root-card-img"></div>
+						<div className="tree-index__root-card-info">
+							<div>
+								{`${formatName(
+									getPersonByID(root.partner1_id, peopleById),
+									"normal",
+								)} &
+								${formatName(
+									getPersonByID(root.partner2_id, peopleById),
+									"normal",
+								)}`}
+							</div>
+							<div>XX Generationen &middot; XX Nachkommen</div>
+						</div>
+					</div>
+					<div className="tree-index__root-card-right">
+						<Link to={`/tree/${root.id}`}>Ansehen &rarr;</Link>
+					</div>
+				</div>
 			))}
-		</ul>
+		</div>
 	);
 }
